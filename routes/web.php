c76c554492f427eac430e24606afb2f47a263228
Sonawap <paulsola79@gmail.com>
2021-09-03 16:56:43 -0700

    ssearchable

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     $users = User::all();
-//     return view('tables.index', ['users' => $users]);
-// });
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('search.index');
 });
 
 // Auth::routes();
